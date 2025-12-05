6b538e33462e1af20f644fbd7737caa29f414cef
kellenwiltshire <kellenwiltshire@outlook.com>
2025-12-05 09:34:19 -0500

    chore: prettier and linting updates

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fillsReducer from "./fills/fillsSlice";
-import cylinderReducer from "./cylinder/cylinderSlice";
-import clientReducer from "./client/clientSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import fillsReducer from './fills/fillsSlice'
+import cylinderReducer from './cylinder/cylinderSlice'
+import clientReducer from './client/clientSlice'
 
 export const store = configureStore({
-  reducer: {
-    fills: fillsReducer,
-    cylinders: cylinderReducer,
-    clients: clientReducer,
-  },
-});
+	reducer: {
+		fills: fillsReducer,
+		cylinders: cylinderReducer,
+		clients: clientReducer,
+	},
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
