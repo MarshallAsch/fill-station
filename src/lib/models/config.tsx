cdf9c7eb3b2dd920a89a07193e3154311f023a91
Marshall Asch <marshallasch@gmail.com>
2025-12-08 14:56:47 -0500

    feat: add inital config setup

--- src/lib/models/config.tsx
+++ src/lib/models/config.tsx
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize'
+import nconf from '../config'
+
 
 export const sequelize = new Sequelize({
 	dialect: 'mariadb',
-	host: 'localhost',
-	database: 'fills',
-	username: 'fills',
-	password: 'fills',
+	host: nconf.get('database:host'),
+	database: nconf.get('database:database'),
+	username: nconf.get('database:username'),
+	password: nconf.get('database:password'),
 })
