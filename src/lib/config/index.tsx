b04e9df79c57502d2555138c6811a3c92485125f
Marshall Asch <marshallasch@gmail.com>
2025-12-16 19:38:32 -0500

    lint: config

--- src/lib/config/index.tsx
+++ src/lib/config/index.tsx
@@ -1,34 +1,32 @@
+import nconf from 'nconf-esm'
+import { load, dump } from 'js-yaml'
+import path from 'path'
 
+nconf
+	.use('memory')
+	.argv({ parseValues: true })
+	.env({ lowerCase: true, parseValues: true })
+	.file('container', {
+		file: path.join('/config', 'config.yaml'),
+		format: {
+			parse: load,
+			stringify: dump,
+		},
+	})
+	.file('local', {
+		file: path.join(__dirname, '..', '..', '..', 'config.yaml'),
+		format: {
+			parse: load,
+			stringify: dump,
+		},
+	})
+	.defaults({
+		database: {
+			host: 'localhost',
+			database: 'fills',
+			username: 'fills',
+			password: 'fills',
+		},
+	})
 
-import nconf from "nconf-esm";
-import {load,dump } from "js-yaml";
-import path from 'path';
-
- nconf
-  .use('memory')
-  .argv({parseValues: true})
-  .env({lowerCase: true, parseValues: true})
-  .file('container', {
-      file: path.join('/config', 'config.yaml'),
-      format: {
-        parse: load,
-        stringify: dump,
-      }
-  })
-  .file('local', {
-      file: path.join( __dirname, '..', '..', '..', 'config.yaml'),
-      format: {
-        parse: load,
-        stringify: dump,
-      }
-  })
-  .defaults({
-  database: {
-    host: 'localhost',
-    database: 'fills',
-    username: 'fills',
-    password: 'fills'
-  }
-});
-
-export default nconf;
+export default nconf
