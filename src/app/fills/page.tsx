d0c15a4fc68e9c719432551c7c2d3868e1b06601
Marshall Asch <marshallasch@gmail.com>
2026-02-14 23:21:10 -0500

    dont use redux for keeping track of the selected client

--- src/app/fills/page.tsx
+++ src/app/fills/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import DatePicker from '@/components/UI/FormElements/DatePicker'
 import ClientPicker from '@/components/UI/FormElements/ClientPicker'
 import FillsTable from '@/components/Fills/FillsTable'
+import { Client } from '@/types/client'
 
 export default function Fills() {
+	const [client, setClient] = useState<Client>()
+
 	return (
 		<div className='max-w-7xl'>
 			<div className='my-4 flex flex-col items-center justify-center gap-3'>
@@ -13,10 +16,10 @@ export default function Fills() {
 
 				<div className='flex gap-2'>
 					<DatePicker title='Fill Date' name='fillDate' id='fill-date' />
-					<ClientPicker />
+					<ClientPicker onChange={(c) => setClient(c)} />
 				</div>
 
-				<FillsTable />
+				<FillsTable client={client} />
 			</div>
 		</div>
 	)
