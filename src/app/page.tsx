52149e1fd8eebf87b9b8ce58351b2f9bd0098e0d
Marshall Asch <marshallasch@gmail.com>
2026-02-16 23:16:30 -0500

    add simple services list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import { EyeIcon, TableCellsIcon } from '@heroicons/react/24/outline'
+import {
+	EyeIcon,
+	TableCellsIcon,
+	SparklesIcon,
+	UserGroupIcon,
+} from '@heroicons/react/24/outline'
 import React from 'react'
 import Link from 'next/link'
 import AirTank from '@/icons/AirTank'
@@ -7,7 +12,7 @@ export default function Home() {
 	return (
 		<div className='text-center'>
 			<h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>
-				Marshalls Dive station tracking site
+				Dive Tec Fill Station Tracking
 			</h1>
 			<p className='mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl'>
 				A simple site for keeping track of service that was done at the fill
@@ -36,6 +41,47 @@ export default function Home() {
 						<TableCellsIcon />
 						<p>History</p>
 					</Link>
+				</div>
+			</div>
+			<h3 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl'>
+				Services Offered
+			</h3>
+			<div className='mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8'>
+				<div className='-mx-6 grid grid-cols-3 gap-1 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-5'>
+					<div>
+						<AirTank />
+						<p>Air</p>
+					</div>
+
+					<div>
+						<AirTank />
+						<p>Nitrox</p>
+					</div>
+
+					<div>
+						<AirTank />
+						<p>Oxygen</p>
+					</div>
+
+					<div>
+						<AirTank />
+						<p>Helium</p>
+					</div>
+
+					<div>
+						<EyeIcon />
+						<p>Visual Cylinder Inspections</p>
+					</div>
+
+					<div>
+						<SparklesIcon />
+						<p>Oxygen Clean Cylinder + valves</p>
+					</div>
+
+					<div>
+						<UserGroupIcon />
+						<p>Training</p>
+					</div>
 				</div>
 			</div>
 		</div>
