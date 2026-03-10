54cf90c013879b5fb54c21bf8ce8f539c3c1833b
kellenwiltshire <kellenwiltshire@outlook.com>
2026-03-10 15:23:31 -0400

    fix: cylinder overflow

--- src/components/Modals/CylinderModal.tsx
+++ src/components/Modals/CylinderModal.tsx
@@ -86,7 +86,7 @@ const CylinderModal = ({
 	}
 	return (
 		<Transition show={true} as={Fragment}>
-			<Dialog as='div' onClose={handleClose} className='relative z-50'>
+			<Dialog as='div' onClose={handleClose} className='relative z-9999'>
 				<TransitionChild
 					as={Fragment}
 					enter='ease-out duration-300'
@@ -98,8 +98,8 @@ const CylinderModal = ({
 				>
 					<div className='bg-opacity-75 fixed inset-0 bg-gray-500 transition-opacity' />
 				</TransitionChild>
-				<div className='fixed inset-0 z-50 overflow-y-auto'>
-					<div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+				<div className='fixed inset-0 z-40 overflow-y-auto'>
+					<div className='relative flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
 						<TransitionChild
 							as={Fragment}
 							enter='ease-out duration-300'
@@ -109,7 +109,7 @@ const CylinderModal = ({
 							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 						>
-							<DialogPanel className='relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
+							<DialogPanel className='transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
 								<DialogTitle>{title}</DialogTitle>
 								<form className='flex flex-col gap-4' action={handleSubmit}>
 									<DialogTitle>{description}</DialogTitle>
