7b6b0ed08b9e3522a53ae42cdba9722d49f1be80
kellenwiltshire <kellenwiltshire@outlook.com>
2026-03-14 15:18:45 -0400

    update contact page styling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,6 @@ import Button from '@/components/UI/Button'
 import TextArea from '@/components/UI/FormElements/TextArea'
 import TextInput from '@/components/UI/FormElements/TextInput'
 import { newContact } from '../_api'
-import { toast } from 'react-toastify'
 
 export default function Contact() {
 	const handleSubmit = async (form: FormData) => {
@@ -19,46 +18,58 @@ export default function Contact() {
 			toast.error(`Error: ${data}`)
 		}
 	}
-
 	return (
-		<div className='max-w-7xl'>
-			<div className='flex flex-col overflow-auto'>
-				<div className='flex flex-col items-center justify-center gap-3 py-6'>
-					<h1 className='text-4xl font-semibold text-gray-900'>Contact Us</h1>
-				</div>
+		<div className='relative flex max-w-7xl flex-row-reverse items-center'>
+			<div className='flex h-full w-1/2 items-center'>
+				{/* TODO Replace with a local image and use Next Image component */}
+				<img
+					alt=''
+					src='https://images.unsplash.com/photo-1699740549075-0a25d76823ac?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+					className='h-64 w-full bg-gray-50 object-cover sm:h-80 lg:h-3/4'
+				/>
+			</div>
+			<div className='flex h-full w-1/2 flex-col items-center justify-center'>
+				<div className='mx-auto flex max-w-xl flex-col gap-3 lg:mx-0 lg:max-w-lg'>
+					<h2 className='text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl'>
+						Get in touch
+					</h2>
+					<p className='mt-2 text-lg/8 text-gray-600'>
+						Proin volutpat consequat porttitor cras nullam gravida at orci
+						molestie a eu arcu sed ut tincidunt magna.
+					</p>
+					<form action={handleSubmit} className='flex flex-col gap-3'>
+						<TextInput
+							autoFocus
+							type='text'
+							id='name'
+							name='name'
+							ariaLabel='Name'
+							placeholder='Who are you'
+						/>
 
-				<form action={handleSubmit}>
-					<TextInput
-						autoFocus
-						type='text'
-						id='name'
-						name='name'
-						ariaLabel='Name'
-						placeholder='Who are you'
-					/>
+						<TextInput
+							autoFocus
+							type='email'
+							id='email'
+							name='email'
+							ariaLabel='Email'
+							placeholder='Email Address'
+						/>
 
-					<TextInput
-						autoFocus
-						type='email'
-						id='email'
-						name='email'
-						ariaLabel='Email'
-						placeholder='Email Address'
-					/>
+						<TextArea
+							id='message'
+							name='message'
+							placeholder='Message'
+							ariaLabel='Message'
+						/>
 
-					<TextArea
-						id='message'
-						name='message'
-						placeholder='Message'
-						ariaLabel='Message'
-					/>
-
-					<div className='flex w-full justify-end py-10'>
-						<div className='w-40'>
-							<Button type='submit'>Submit</Button>
+						<div className='flex w-full justify-end py-10'>
+							<div className='w-40'>
+								<Button type='submit'>Submit</Button>
+							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
 	)
